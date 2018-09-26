b3a1389dff6384f32232b8ac6c50844e3fea7c32
paul <paulwelch@outlook.com>
2018-09-26 08:46:04 -0700

    work in process for parsing and output formatting

--- main.go
+++ main.go
@@ -30,12 +30,15 @@ import (
 func main() {
 	var config string
 	var filter string
+	var raw bool
+	var json bool
 
 	//TODO: expand on filter flag - possible regex, state values, by namespace, by label
 	//TODO: maybe match kubectl command pattern: get, describe, watch
 	//      ideas for metric views - rolling update state; deployment state, hpa's, jobs, etc
-	//TODO: add output format options
+	//TODO: add output format options (raw, json, table)
 	//TODO: add reasonable defaults with no command or flags - maybe a 'top' display
+	//TODO: list of metrics with help text
 
 	app := cli.NewApp()
 	app.Name = "kubestate"
@@ -49,10 +52,20 @@ func main() {
 			Destination: &config,
 		},
 		cli.StringFlag{
-			Name:        "filter",
+			Name:        "filter, f",
 			Value:       "*",
 			Usage:       "Metric filter to show",
 			Destination: &filter,
+		},
+		cli.BoolFlag{
+			Name:        "raw, r",
+			Usage:       "Show raw response data format",
+			Destination: &raw,
+		},
+		cli.BoolFlag{
+			Name:        "json, j",
+			Usage:       "Show JSON format",
+			Destination: &json,
 		},
 	}
 
@@ -67,7 +80,7 @@ func main() {
 			return err
 		}
 
-		//determine if kube-state-metrics is available service
+		//determine if kube-state-metrics service is available
 		stateServiceFound := false
 		svcs, err := k8sclient.CoreV1().Services("kube-system").List(v1.ListOptions{})
 		if err != nil {
@@ -90,15 +103,26 @@ func main() {
 			return cli.NewExitError("Error: kube-state-metrics service is not healthy", 98)
 		}
 
-		//request protobuf using Accept header
-		const acceptHeader = `application/vnd.google.protobuf;proto=io.prometheus.client.MetricFamily;encoding=delimited;q=0.7,text/plain;version=0.0.4;q=0.3`
-
 		//get kube-state-metrics raw data and parse
-		r = k8sclient.RESTClient().Get().SetHeader("Accept", acceptHeader).RequestURI(cfg.Host + "/api/v1/namespaces/kube-system/services/kube-state-metrics:http-metrics/proxy/metrics").Do()
+		if raw {
+			//want raw text data, so no protobuf header
+			r = k8sclient.RESTClient().Get().RequestURI(cfg.Host + "/api/v1/namespaces/kube-system/services/kube-state-metrics:http-metrics/proxy/metrics").Do()
+		} else {
+			//request protobuf using Accept header
+			const acceptHeader= `application/vnd.google.protobuf;proto=io.prometheus.client.MetricFamily;encoding=delimited;q=0.7,text/plain;version=0.0.4;q=0.3`
+			r = k8sclient.RESTClient().Get().SetHeader("Accept", acceptHeader).RequestURI(cfg.Host + "/api/v1/namespaces/kube-system/services/kube-state-metrics:http-metrics/proxy/metrics").Do()
+		}
+
 		if r.Error() != nil {
 			return r.Error()
 		}
 		resp, _ = r.Raw()
+
+		if raw {
+			fmt.Println(string(resp))
+			return nil
+		}
+
 
 		//Might be faster with parallel go routine to parse, but with higher complexity.
 		//Only ~100 families, so probably not worth it at this time.
@@ -115,34 +139,41 @@ func main() {
 			metricFamilies = append(metricFamilies, mf)
 
 			if filter == "*" || *mf.Name == filter {
-				fmt.Println("---------------")
-				fmt.Println(*mf.Name)
-				fmt.Println(*mf.Type)
-				fmt.Println(*mf.Help)
 
-				//for debugging
-				for i:=0; i<len(mf.Metric); i++ {
-					for j:=0; j<len((*mf.Metric[i]).Label); j++ {
+				if json {
+					//TODO: output as json format
+					fmt.Println("Not implemented yet")
+				} else {
+					fmt.Println("---------------")
+					fmt.Println(*mf.Name)
+					fmt.Println(*mf.Type)
+					fmt.Println(*mf.Help)
 
-						fmt.Println("---------------")
-						fmt.Printf("Metric %d: Label %d:  %s  value: %s\n", i, j, *mf.Metric[i].Label[0].Name, *mf.Metric[i].Label[0].Value)
+					//for debugging
+					for i:=0; i<len(mf.Metric); i++ {
+						for j:=0; j<len((*mf.Metric[i]).Label); j++ {
 
-						switch *mf.Type {
+							fmt.Println("---------------")
+							fmt.Printf("Metric %d: Label %d:  %s  value: %s\n", i, j, *mf.Metric[i].Label[0].Name, *mf.Metric[i].Label[0].Value)
 
-							case dto.MetricType_COUNTER:
-								fmt.Printf("Counter Value: %f", *mf.Metric[i].Counter.Value)
+							switch *mf.Type {
 
-							case dto.MetricType_GAUGE:
-								fmt.Printf("Gauge Value: %f", *mf.Metric[i].Gauge.Value)
+								case dto.MetricType_COUNTER:
+									fmt.Printf("Counter Value: %f", *mf.Metric[i].Counter.Value)
 
-							case dto.MetricType_SUMMARY:
-								fmt.Println(*mf.Metric[i].Summary.Quantile[0].Value)
-								fmt.Println(*mf.Metric[i].Summary.Quantile[0].Quantile)
-								fmt.Println(*mf.Metric[i].Summary.SampleCount)
-								fmt.Println(*mf.Metric[i].Summary.SampleSum)
+								case dto.MetricType_GAUGE:
+									fmt.Printf("Gauge Value: %f", *mf.Metric[i].Gauge.Value)
 
+								case dto.MetricType_SUMMARY:
+									fmt.Println(*mf.Metric[i].Summary.Quantile[0].Value)
+									fmt.Println(*mf.Metric[i].Summary.Quantile[0].Quantile)
+									fmt.Println(*mf.Metric[i].Summary.SampleCount)
+									fmt.Println(*mf.Metric[i].Summary.SampleSum)
+
+							}
 						}
 					}
+
 				}
 
 			}
