c788dffbaeb90e9cb005ceb4a35586e7e0a6cd96
paul <paulwelch@outlook.com>
2018-10-30 19:06:40 -0700

    first pass at top deployments

--- cmd/top.go
+++ cmd/top.go
@@ -22,6 +22,7 @@ import (
 //TOP Ideas
 // top rollup by: Deployment / RC/RS / Service / Pod, Job/CronJob, Resource Quotas, HPA (network??), Storage (may not have right metrics for it)
 
+//pods---
 type rowKey struct {
 	namespace, pod, container string
 }
@@ -43,7 +44,6 @@ type sortKey struct {
 type sortedKeys []*sortKey
 
 // sort.Interface implementation
-
 func (s sortedKeys) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
@@ -59,7 +59,41 @@ func (s sortedKeys) Less(i, j int) bool {
 	}
 	return false
 }
+//---pods
 
+//deployments---
+type deployKey struct {
+	namespace, deployment string
+}
+
+type deploy struct {
+	requested, available, unavailable float64
+}
+
+type deploySortKey struct {
+	key deployKey
+	value float64
+}
+
+type sortedDeployKeys []*deploySortKey
+
+// sort.Interface implementation
+func (s sortedDeployKeys) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortedDeployKeys) Len() int {
+	return len(s)
+}
+
+func (s sortedDeployKeys) Less(i, j int) bool {
+
+	if s[i].value < s[j].value {
+		return true
+	}
+	return false
+}
+//---deployments
 
 func Top(c *cli.Context) error {
 
@@ -70,14 +104,67 @@ func Top(c *cli.Context) error {
 		return err
 	}
 
-	table := make(map[rowKey]*row)
-	nodes := make(map[string]*node)
-
 	//TODO: do we aggregate by node?
 	switch c.Command.Name {
 	case "deployments":
-		//TODO: deployment pods - desired (kube_deployment_spec_replicas), available(kube_deployment_status_replicas_available), unavailable(kube_deployment_status_replicas_unavailable)
+		//TODO: add rolling update metrics
+		table := make(map[deployKey]*deploy)
+
+		for i := 0; i < len(metricFamilies); i++ {
+
+			var ns, d string
+
+			if *metricFamilies[i].Name == "kube_deployment_spec_replicas" ||
+				*metricFamilies[i].Name == "kube_deployment_status_replicas_available" ||
+				*metricFamilies[i].Name == "kube_deployment_status_replicas_unavailable" {
+				for _, f := range metricFamilies[i].Metric {
+
+					for _, l := range f.Label {
+						switch *l.Name {
+						case "namespace":
+							ns = *l.Value
+						case "deployment":
+							d = *l.Value
+						}
+					}
+
+					if table[deployKey{ns, d}] == nil {
+						table[deployKey{ns, d}] = &deploy{}
+					}
+
+					switch *metricFamilies[i].Name {
+					case "kube_deployment_spec_replicas":
+						table[deployKey{ns, d}].requested += *f.Gauge.Value
+					case "kube_deployment_status_replicas_available":
+						table[deployKey{ns, d}].available += *f.Gauge.Value
+					case "kube_deployment_status_replicas_unavailable":
+						table[deployKey{ns, d}].unavailable += *f.Gauge.Value
+					}
+				}
+			}
+		}
+
+		s := make(sortedDeployKeys, 0, len(table))
+		for k, v := range table {
+			s = append(s, &deploySortKey{k, v.requested})
+		}
+		sort.Sort(sort.Reverse(s))
+
+		w := new(tabwriter.Writer)
+		w.Init(os.Stdout, 4, 1, 1, ' ', 0)
+
+		fmt.Fprintf(w, "%s\t%s\t%s\n", "Namespace", "Deployment", "(Req / Avail / Unavail)")
+
+		for _, v := range s {
+			fmt.Fprintf(w, "%s\t%s\t(%.0f / %.0f / %.0f)\n", v.key.namespace, v.key.deployment, table[v.key].requested, table[v.key].available, table[v.key].unavailable)
+		}
+
+		w.Flush()
+
 	case "pods":
+		table := make(map[rowKey]*row)
+		nodes := make(map[string]*node)
+
 		for i := 0; i < len(metricFamilies); i++ {
 
 			var ns, po, co, re, n string
